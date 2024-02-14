7dcd50fe4e82bb82e830e559d28c77213bfaac8f
rulanugrh <nextarituenak01@gmail.com>
2024-02-14 21:01:25 +0700

    feat: comment services

--- clean-arch/src/service/comment.ts
+++ clean-arch/src/service/comment.ts
@@ -11,21 +11,58 @@ export interface CommentService {
 class comment {
     repo: CommentRepository
 }
-export class NewCommentService implements CommentRepository {
+export class NewCommentService implements CommentService {
     comm: comment
     constructor(repo: CommentRepository) {
         this.comm.repo = repo
     }
 
     async Create(req: CommentReq): Promise<CreateComment> {
-        
+        try {
+            const data = await this.comm.repo.Create(req)
+            const response: CreateComment = {
+                comment: data.comment,
+                event: data.event.name,
+            }
+
+            return response
+        } catch (error) {
+            throw new Error(`error from srvice create comment :${error}`)
+        }   
     }
 
     async Find(): Promise<GetComment[]> {
-        
+        try {
+            const data = await this.comm.repo.Find()
+            var response: GetComment[]
+            for (const d of data) {
+                var res: GetComment = {
+                    comment: d.comment,
+                    event: d.event.name,
+                    id: d.id,
+                }
+
+                response.push(res)
+            }
+
+            return response
+        } catch (error) {
+            throw new Error(`error from service find comment :${error}`)
+        }
     }
 
-    async FindID(id: string): Promise<GetComment> {
-        
+    async FindId(id: string): Promise<GetComment> {
+        try {
+            const data = await this.comm.repo.FindID(id)
+            const response: GetComment = {
+                id: data.id,
+                event: data.event.name,
+                comment: data.comment
+            }
+
+            return response
+        } catch (error) {
+            throw new Error(`error from service find by id comment :${error}`)
+        }
     }
 }
