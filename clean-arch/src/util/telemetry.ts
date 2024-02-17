eb8504f3e6e63d9f3a573ba0421394743605b310
Pradana <nextarituenak01@gmail.com>
2024-02-17 12:36:23 +0000

    chore: adding instrument for http and express

--- clean-arch/src/util/telemetry.ts
+++ clean-arch/src/util/telemetry.ts
@@ -6,6 +6,8 @@ import {
 } from '@opentelemetry/sdk-metrics';
 import { Resource } from '@opentelemetry/resources';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
+import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
+import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
 import "dotenv/config"
 
 const sdk = new NodeSDK({
@@ -13,6 +15,10 @@ const sdk = new NodeSDK({
         [SemanticResourceAttributes.SERVICE_NAME]: process.env.OTEL_SERVICE_NAME,
         [SemanticResourceAttributes.SERVICE_VERSION]: process.env.OTEL_SERVICE_VERSION
     }),
+    instrumentations : [
+        new ExpressInstrumentation(),
+        new HttpInstrumentation()
+    ],
     traceExporter: new ConsoleSpanExporter(),
     metricReader: new PeriodicExportingMetricReader({
         exporter: new ConsoleMetricExporter()
